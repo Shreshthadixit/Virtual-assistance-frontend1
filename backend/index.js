5f4075345906575280326ea1eaaf658c21f2ec7c
Shreshthadixit <dixitshreshtha88@gmail.com>
2025-10-10 00:06:03 +0530

    u

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,13 @@ import geminiResponse from "./gemini.js"
 
 
 const app=express()
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    // Ab yeh live Vercel URL ko bhi accept karega
+    origin: FRONTEND_URL, 
+    credentials:true
 }))
+
 const port=process.env.PORT || 8000
 app.use(express.json())
 app.use(cookieParser())
